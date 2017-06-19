bab73e427d4ef13d9981df3b70ab0f53bc9c6d22
Steve <svandenbush@geekhive.com>
2017-06-19 14:49:14 -0400

    removed curl dependencies, added listofitems support

--- deploy-sitecorepackage.ps1
+++ deploy-sitecorepackage.ps1
@@ -1,8 +1,8 @@
 <#
     This function uploads & installs the specified Sitecore update package to the given $SiteUrl.
-    It uses cURL (http://curl.haxx.se/) to post a request to a Sitecore website which has Sitecore Ship installed.
     Example usage:
-    .\deploy-sitecorepackage.ps1 mysite.dev "C:\Project\Build\Artifacts\1-mysite-templates.update" 60 300
+    .\deploy-sitecorepackage.ps1 mysite.dev "C:\Project\Build\Artifacts\1-mysite-templates.update" 300 MyUsername MyPassword
+	.\deploy-sitecorepackage.ps1 mysite.dev "C:\Project\Build\Artifacts\1-mysite-templates.update" 300 -ResultsOutputPath "c:/temp/ids.txt"
 #>
 
 Param(
@@ -13,15 +13,41 @@ Param(
     [Parameter(Position=2)]
     [ValidateRange(0, 99999)]
     [int]$ConnectionTimeOutInSeconds = 300,
-    [Parameter(Position=3)]
-    [ValidateRange(0, 99999)]
-    [int]$MaxTimeOutInSeconds = 900
+	[Parameter(Position=3)]
+    [string]$Username,
+	[Parameter(Position=4)]
+    [string]$Password,
+	[Parameter(Position=5)]
+    [string]$ResultsOutputPath
 )
-
+."$PSScriptRoot\multipartFormDataUpload.ps1"
 $fileUploadUrl = "$SiteUrl/services/package/install/fileupload"
-$curlPath = .$PSScriptRoot\get-curlpath.ps1
-$curlCommand= "$curlPath -sS -fail --connect-timeout $ConnectionTimeOutInSeconds --max-time $MaxTimeOutInSeconds --form ""filename=@$UpdatePackagePath"" $fileUploadUrl --progress-bar"
 
-Write-Output "INFO: Starting Invoke-Expression: $curlCommand"
 
-Invoke-Expression $curlCommand
+
+if($Username){
+	$secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
+	$mycreds = New-Object System.Management.Automation.PSCredential ($Username, $secpasswd)
+	Write-Host "POST Fileupload with credentials" -foregroundcolor green
+	$shipResponse = Invoke-MultipartFormDataUpload -InFile $UpdatePackagePath -Uri $fileUploadUrl -Timeout $ConnectionTimeOutInSeconds -Credential $mycreds | ConvertFrom-Json
+}
+else{
+	Write-Host "POST Fileupload" -foregroundcolor green
+	$shipResponse = Invoke-MultipartFormDataUpload -InFile $UpdatePackagePath -Uri $fileUploadUrl -Timeout $ConnectionTimeOutInSeconds  | ConvertFrom-Json
+}
+
+$shipEntries = $shipResponse.Entries
+$items = New-Object System.Collections.Generic.List[string]
+$shipEntries | 
+	ForEach-Object {
+		if($_.ID -ne $null)
+		{
+			Write-Host $_.ID -foregroundcolor cyan
+			$items.Add($_.ID)
+		}
+	}
+	
+if($ResultsOutputPath -ne $null)
+{
+	$items  | out-file $ResultsOutputPath
+}
--- publish-sitecorepackage.ps1
+++ publish-sitecorepackage.ps1
@@ -1,8 +1,8 @@
 <#
     This function "smart" publishes the specified Sitecore target to the given $SiteUrl.
-    It uses cURL (http://curl.haxx.se/) to post a request to a Sitecore website which has Sitecore Ship installed.
     Example usage: 
-    .\publish-sitecorepackage.ps1 mysite.dev "preview,web" "smart" 60 300
+    .\publish-sitecorepackage.ps1 mysite.dev "preview,web" "en" "smart" 300
+	.\publish-sitecorepackage.ps1 mysite.dev "preview,web" "en" "listofitems" 300 MyUserName MyPassword "C:/temp/ids.txt"
 #>
 
 Param(
@@ -10,21 +10,53 @@ Param(
     [string]$SiteUrl,
     [Parameter(Position=1, Mandatory=$true)]
     [string]$PublishTargets = "preview,web",
-	[Parameter(Position=2, Mandatory=$false)]
+	[Parameter(Position=2, Mandatory=$true)]
+    [string]$PublishLanguaes = "en",
+	[Parameter(Position=3, Mandatory=$false)]
     [string]$PublishMode = "smart",
-    [Parameter(Position=3)]
-    [ValidateRange(0, 99999)]
-    [int]$ConnectionTimeOutInSeconds = 300,
     [Parameter(Position=4)]
     [ValidateRange(0, 99999)]
-    [int]$MaxTimeOutInSeconds = 900
+    [int]$ConnectionTimeOutInSeconds = 300,
+	[Parameter(Position=5)]
+    [string]$Username,
+	[Parameter(Position=6)]
+    [string]$Password,
+	[Parameter(Position=7)]
+    [string]$ListOfIDsInputPath
 
 )
+."$PSScriptRoot\multipartFormDataUpload.ps1"
+$publishUrl = "$SiteUrl/services/publish/$PublishMode"
 
-$fileUploadUrl = "$SiteUrl/services/publish/$PublishMode"
-$curlPath = .$PSScriptRoot\get-curlpath.ps1
-$curlCommand= "$curlPath -sS -fail --connect-timeout $ConnectionTimeOutInSeconds --max-time $MaxTimeOutInSeconds -X POST --form ""targets=$PublishTargets"" $fileUploadUrl"
+$targetDatabases = $PublishTargets -split ","
+$targetLanguages = $PublishLanguaes -split ","
+$pubishItems = @{
+		targets = $targetDatabases
+		languages = $targetLanguages
+	}
 
-Write-Output "INFO: Starting Invoke-Expression: $curlCommand"
-
-Invoke-Expression $curlCommand
+if($PublishMode -eq "listofitems")
+{
+	if (-not (Test-Path $ListOfIDsInputPath)) 
+	{
+		throw [System.IO.FileNotFoundException] "listofitems publish: $ListOfIDsInputPath not found."
+	}
+    [Collections.Generic.List[String]]$items = [IO.File]::ReadAllLines($ListOfIDsInputPath) 
+	Write-Host $items -foregroundcolor cyan
+	$pubishItems = @{
+			TargetDatabases = $targetDatabases
+			TargetLanguages = $targetLanguages
+			Items = $items
+		}
+}
+	
+if($Username){
+	$secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
+	$mycreds = New-Object System.Management.Automation.PSCredential ($Username, $secpasswd)
+	Write-Host "POST Publish $PublishMode with credentials" -foregroundcolor green
+	Invoke-RestMethod -Uri $publishUrl -Method POST -ContentType "application/json" -Body ($pubishItems | ConvertTo-Json) -cred $mycreds
+}
+else{
+	Write-Host "POST Publish $PublishMode" -foregroundcolor green
+	Invoke-RestMethod -Uri $publishUrl -Method POST -ContentType "application/json" -Body ($pubishItems | ConvertTo-Json)
+}
